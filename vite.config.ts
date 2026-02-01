6ece06877ce4996565a6ced77fe7e848418b245c
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2026-02-01 09:17:19 +0100

    base=/byob-todo-frontend

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: process.env.VITE_BASE || "/",
   plugins: [devtools(), solidPlugin()],
   server: {
     port: 3000,
